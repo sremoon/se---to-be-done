01b6ef5685409ceeb1afd449452d44259f83cef6
sremoon <1606338033@qq.com>
2022-11-02 08:25:46 -0700

    judge

---
+++ judge.h
@@ -0,0 +1,11 @@
+#include "program.h"
+#include "test.h"
+
+class Machine_Judge {
+    private:
+    public:
+        void Judge(Test &tt, All_program &Programs) {
+            int l = 
+            
+        }
+} ;
